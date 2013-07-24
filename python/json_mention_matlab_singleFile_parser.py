60b4ff95fbe707649d785e5fc6bf40d82c00a3e3
Dinos <dinos66@yahoo.com>
2013-07-24 19:20:35 +0300

    improved python files

--- python/json_mention_matlab_singleFile_parser.py
+++ python/json_mention_matlab_singleFile_parser.py
@@ -1,21 +1,23 @@
-# -*- coding: cp1253 -*-
 #-------------------------------------------------------------------------------
-# Name:         json_parser_singlefile
-# Purpose:      parsing data from a json file to a form:
-#               author1 mentioned1 "timestamp"\n
-#               author1 mentioned2,... "timestamp"\n
-#               creating a single file without the text content to render the
-#               matlab functions more efficient.
-# Author:       konkonst
+# Name:          
+# Purpose:       parsing data from a json file to a form:
+#                author1 mentioned1 unixTimestamp\n
+#                author1 mentioned2,... unixTimestamp\n
+#                creating a single file without the text content to render the
+#                matlab functions more efficient.
+# Required libs: wxPython GUI toolkit, python-dateutil
+# Author:        konkonst
 #
-# Created:      31/05/2013
-# Copyright:    (c) ITI (CERTH) 2013
-# Licence:      <apache licence 2.0>
+# Created:       31/05/2013
+# Copyright:     (c) ITI (CERTH) 2013
+# Licence:       <apache licence 2.0>
 #-------------------------------------------------------------------------------
 import json
 import codecs
 import os,glob
 import wx
+import dateutil.parser
+import time
 
 # User selects dataset folder
 app = wx.PySimpleApp()
@@ -30,23 +32,28 @@ dialog = wx.DirDialog(None, "Please select your target folder:",defaultPath=targ
 if dialog.ShowModal() == wx.ID_OK:
     target_path= dialog.GetPath()
 dialog.Destroy()
-#Parsing commences
+###Parsing commences###
 my_txt=open(target_path+"/authors_mentions_time.txt","w")
 for filename in glob.glob(dataset_path+"/*.json"):
     print(filename)
     my_file=open(filename,"r")
     read_line=my_file.readline()
+    ustr_to_load = unicode(read_line, 'iso-8859-15')
     while read_line:
-        json_line=json.loads(read_line)##,encoding="cp1252")#.decode('utf-8','replace')
+        json_line=json.loads(ustr_to_load)
         if "delete" in json_line or "scrub_geo" in json_line or "limit" in json_line:
             read_line=my_file.readline()
+            ustr_to_load = unicode(read_line, 'iso-8859-15')
             continue
         else:     
             if json_line["entities"]["user_mentions"]:
                 len_ment=len(json_line["entities"]["user_mentions"])
-                for i in range(len_ment):##mentions.append(json_line["entities"]["user_mentions"][i]["screen_name"]) ##my_text=str(json_line["text"].encode('ascii','replace').replace('\n', ''))
-                    my_txt.write(json_line["user"]["screen_name"]+"\t" + json_line["entities"]["user_mentions"][i]["screen_name"]+"\t"+"\""+json_line["created_at"]+"\""+"\n")            
+                dt=dateutil.parser.parse(json_line["created_at"])
+                mytime=int(time.mktime(dt.timetuple()))
+                for i in range(len_ment):
+                    my_txt.write(json_line["user"]["screen_name"]+"\t" + json_line["entities"]["user_mentions"][i]["screen_name"]+"\t"+str(mytime)+"\n")            
         read_line=my_file.readline()
+        ustr_to_load = unicode(read_line, 'iso-8859-15')
     else:
         my_file.close()
 my_txt.close()
